a8860c495b8e0fe34ebcfa4289b5e5019ce41354
Subayyal Ayub <113839899+subayyalawan@users.noreply.github.com>
2023-12-12 19:07:01 +0500

    second Commit

--- src/pages/AnimatedNav.jsx
+++ src/pages/AnimatedNav.jsx
@@ -18,7 +18,7 @@ const AnimatedNav = () => {
         <div
           className={` ${
             showNav ? "rotate-[-20deg]" : "rotate-[0deg]"
-          } wrapper origin-top-left transition-transform duration-500 ease-linear w-[90%] mx-auto`}
+          } wrapper origin-top-left transition-transform duration-500 ease-linear bg-white`}
         >
           <div className="circle-container">hello world Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque minima culpa voluptas iure nesciunt consectetur enim dolorem. Quibusdam ipsam quis reprehenderit voluptatum nulla ratione odio molestiae itaque veritatis, nihil maxime consequuntur officiis cum perspiciatis dignissimos consectetur quos labore maiores distinctio eos dolor ullam placeat? Temporibus maiores quos alias autem architecto minima aliquam esse enim aliquid optio, asperiores porro consectetur, corporis beatae delectus, sint soluta nemo culpa maxime suscipit nihil voluptates? Iusto porro praesentium officiis illum dicta eaque optio. Porro eligendi aliquam, mollitia nihil facere atque? Culpa repellat eius enim iusto, impedit consequuntur similique velit perferendis et non. A, dolores explicabo?</div>
         </div>
